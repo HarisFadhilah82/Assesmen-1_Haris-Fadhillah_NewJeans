94d866e7651344d7e866ef847230a77f0a83d197
Muhammad Haris Fadhilah <jepangpubgkorea@gmail.com>
2025-06-17 18:38:00 +0700

    Membuat navigasi dengan tombol Up

--- app/src/main/java/com/haris0035/newjeans/ui/theme/screen/AboutScreen.kt
+++ app/src/main/java/com/haris0035/newjeans/ui/theme/screen/AboutScreen.kt
@@ -2,7 +2,11 @@ package com.haris0035.newjeans.ui.theme.screen
 
 import android.content.res.Configuration
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -13,15 +17,28 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.NavHost
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import com.haris0035.newjeans.R
 import com.haris0035.newjeans.ui.theme.NewJeansTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun AboutScreen() {
+fun AboutScreen(navController: NavHostController) {
     Scaffold(
         topBar = {
             TopAppBar(
+                navigationIcon = {
+                    IconButton(onClick = { navController.popBackStack() }) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = stringResource(R.string.kembali),
+                            tint = MaterialTheme.colorScheme.primary
+                        )
+                    }
+                },
                 title = {
                     Text(text = stringResource(id = R.string.tentang_aplikasi))
                 },
@@ -47,6 +64,9 @@ fun AboutScreen() {
 @Composable
 fun AboutScreenPreview() {
     NewJeansTheme {
-        AboutScreen()
+        AboutScreen(
+            rememberNavController(
+            )
+        )
     }
 }
